2e30e22f44b15d48cf11fbf1af47bc4b354f9d0d
Patrick-052 <muiruripatrick052@gmail.com>
2023-08-23 23:24:20 +0300

    Drafts

---
+++ drafts/0x02-cli/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Creating a CLI which is the main entry
+   point to the PataNgoma Auto-tagging tool"""
+
+import click
+from tags import MetaData
+
+
+@click.group()
+def cli():
+    """Nesting all other commands"""
+    click.echo("Welcome")
+    click.echo("Enjoy personalizing your Music files")
+
+
+@cli.command()
+def tag():
+    """Responding to users needs"""
+    choices = [
+        "View existing tags",
+        "Extract new tags",
+        "Edit tags"
+    ]
+
+    click.echo("")
+    click.echo("Provide file path to Manipulate tags ...")
+    file_path = click.prompt("File Path", type=click.Path(exists=True))
+    click.echo("")
+    click.echo("Select an action:")
+    for index, choice in enumerate(choices, start=1):
+        click.echo(f"{index}: {choice}")
+
+    choice = click.prompt("Choice", type=click.IntRange(min=1,
+                                                        max=len(choices)))
+
+    if choice == 1:
+        meta = MetaData(file_path)
+        meta.show_metadata()
+
+    elif choice == 2:
+        click.echo("Edit tags ...")
+
+    elif choice == 3:
+        click.echo("Search tags ...")
+
+
+if __name__ == '__main__':
+    cli()
---
+++ drafts/0x02-cli/tags.py
@@ -0,0 +1,46 @@
+# metadata.py
+from mediafile import MediaFile
+
+
+class BaseModel:
+
+    def __init__(self, file_path):
+        self.file = file_path
+        self.metadata = MediaFile(file_path)
+
+    def as_dict(self):
+        return dict(self.metadata.as_dict())
+
+    def show_all(self):
+        print(f"Metadata of {self.file}:")
+        for key, value in self.as_dict().items():
+            print(f"{key}: {value}")
+
+    def show_metadata(self):
+        print(f"Metadata of {self.file}:")
+        for key, value in self.as_dict().items():
+            if value is not None:
+                print(f"{key}: {value}")
+
+    def update_metadata(self, updates):
+        for update in updates:
+            key, value = update.split("=")
+            if hasattr(self.metadata, key):
+                setattr(self.metadata, key, value)
+            else:
+                print(f"Invalid metadata field: {key}")
+
+    def save(self):
+        self.metadata.save()
+
+
+class TrackInfo(BaseModel):
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self.load_metadata()
+
+    def load_metadata(self):
+        self.title = self.metadata.title
+        self.artist = self.metadata.artist
+        self.album = self.metadata.album
+        self.genres = self.metadata.genres
