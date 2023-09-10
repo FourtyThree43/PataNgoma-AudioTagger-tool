eeab4d48c4e13e1eab310cda7fd6e367789f8b7d
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-09-10 20:42:26 +0300

    see diff

--- drafts/0x04-cli/interactive.py
+++ drafts/0x04-cli/interactive.py
@@ -2,13 +2,24 @@ import os
 import sys
 import click
 from InquirerPy import inquirer
+from mediafile import MediaFile
 from dotenv import load_dotenv
 
-load_dotenv()
-music_path = f"{os.path.expanduser('~')}/{os.getenv('MUSIC_PATH')}"
+
+def set_default_path():
+    load_dotenv()
+    tail = os.getenv("MUSIC_PATH")
+
+    if tail:
+        music_path = f"{os.path.expanduser('~')}{os.path.sep}{tail}"
+    else:
+        click.secho("Path to music directory not set, defaulting to current working directory",
+                    fg="yellow")
+        music_path = os.getcwd()
+    return music_path
 
 
-def interactive_selection():
+def interactive_selection(music_path):
     files = [
         i for i in os.listdir(music_path)
         if not os.path.isdir(f"{music_path}/{i}")
@@ -22,16 +33,21 @@ def interactive_selection():
     return os.path.join(music_path, files[index])
 
 
+def file_handler(filename):
+    pass
+
+
 def main():
+    music_path = set_default_path()
     if len(sys.argv) < 2:
         print("No file selected")
-        filename = interactive_selection()
+        filename = interactive_selection(music_path)
     else:
         filename = sys.argv[1]
 
     if os.path.isdir(filename):
         click.secho(f"'{filename}' is a directory", fg="yellow")
-        filename = interactive_selection()
+        filename = interactive_selection(music_path)
 
     elif not os.path.exists(filename):
         if os.path.sep not in filename:
@@ -40,13 +56,13 @@ def main():
             if os.path.exists(filename):
                 if os.path.isdir(filename):
                     click.secho(f"'{filename}' is a directory", fg="yellow")
-                    filename = interactive_selection()
+                    filename = interactive_selection(music_path)
             else:
                 click.secho(f"The file '{filename}' does not exist", fg="bright_red")
-                filename = interactive_selection()
+                filename = interactive_selection(music_path)
         else:
             click.secho(f"The file '{filename}' does not exist", fg="bright_red")
-            filename = interactive_selection()
+            filename = interactive_selection(music_path)
 
     click.secho(f"The file '{filename}' has been selected", fg="bright_green")
 
